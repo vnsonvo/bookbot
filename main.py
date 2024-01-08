1fbbf3dffe0205eb427f777d85530bd521a09ba9
Son Vo <vnsonvo@gmail.com>
2024-01-08 20:13:10 +0700

    print report about document

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ def main():
         print("Number of words:", num_of_words)
         letter_dict = count_letter(file_content)
         print("Letter dict:", letter_dict)
+        print_report(num_of_words, letter_dict)
 
 def count_word(content):
     words = content.split()
@@ -21,6 +22,21 @@ def count_letter(content):
         else:
             letter_dict[lowercase_char] += 1
     return letter_dict
-        
+
+def sort_elem(el):
+    return el[1]
+
+def print_report(num_of_words, letter_dict):
+    print(f"{num_of_words} words found in the document\n")
+    letter_list = []
+    for key in letter_dict:
+        if key.isalpha():
+            letter_list.append((key, letter_dict[key]))
+    letter_list.sort(reverse=True, key=sort_elem)
+    for letter in letter_list:
+        print(f"The '{letter[0]}' character was found {letter[1]} times")
+
+    print("--- End Report ---")
+
 
 main()
