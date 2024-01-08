9af79c03176ba9b5bd6f38fbbc29d2e6afacde1f
Son Vo <vnsonvo@gmail.com>
2024-01-08 19:55:45 +0700

    count number of char appear in content

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ def main():
         print(file_content)
         num_of_words = count_word(file_content)
         print("Number of words:", num_of_words)
+        letter_dict = count_letter(file_content)
+        print("Letter dict:", letter_dict)
 
 def count_word(content):
     words = content.split()
     return len(words)
+
+def count_letter(content):
+    letter_dict = {}
+    content_without_whitespace = content.strip()
+    for char in content_without_whitespace:
+        lowercase_char = char.lower()
+        if lowercase_char not in letter_dict:
+            letter_dict[lowercase_char] = 1
+        else:
+            letter_dict[lowercase_char] += 1
+    return letter_dict
+        
 
 main()
